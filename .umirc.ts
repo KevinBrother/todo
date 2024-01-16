11f8b91f8ada6b570fa316e6c0b3d2da5fac9b68
caojunjie <1301239018@qq.com>
2024-01-16 15:41:21 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
+    { path: "/test/tailwind", component: "tests/tailwind" },
+  ],
+  npmClient: 'pnpm',
+});
---
+++ src/pages/tests/tailwind.tsx
@@ -0,0 +1,6 @@
+
+export default function Tailwind() {
+  return (
+    <div>tailwind</div>
+  )
+}
