024bf7171663893e558c4b1986ea95e740911d04
caojunjie <1301239018@qq.com>
2024-01-16 19:18:31 +0800

    test(layout) 自适应布局案例

--- src/pages/tests/tailwind.tsx
+++ src/pages/tests/tailwind.tsx
@@ -1,6 +1,19 @@
 
+// import './index.scss';
+
 export default function Tailwind() {
   return (
-    <div>tailwind</div>
+    <div className="outer-container md:flex-row">
+        <div className='border-2 md:flex-1 p-1 flex flex-wrap'>
+            <div className='border-2 flex-1'>firstfirstfirstfirstfirst</div>
+            <div  className='border-2 flex-1'>secondsecondsecondsecondsecond</div>
+            <div  className='border-2 flex-1'>thirdthirdthirdthirdthird</div>
+        </div>
+        <div className='border-2 md:flex-1 p-1'>
+            <div  className='border-2 flex-1'>first</div>
+            <div  className='border-2 flex-1'>second</div>
+            <div  className='border-2 flex-1'>third</div>
+        </div>
+    </div>
   )
 }
