91c351b4b4f486e2de02477b8afa4fca54d7d3d7
caojunjie <1301239018@qq.com>
2024-01-17 10:58:06 +0800

    feat(page) 添加todo列表和详情界面

--- .umirc.ts
+++ .umirc.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from "umi";
+import { defineConfig } from 'umi';
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-    { path: "/test/tailwind", component: "tests/tailwind" },
+    { path: '/', component: 'index' },
+    { path: '/docs', component: 'docs' },
+    { path: '/test/tailwind', component: 'tests/tailwind' },
+    { path: '/todo/pages/task-detail', component: 'todo/pages/task-detail' },
+    { path: '/todo/pages/task-list', component: 'todo/pages/task-list' },
   ],
 
-  npmClient: "pnpm",
+  npmClient: 'pnpm',
   tailwindcss: {},
-  plugins: ["@umijs/plugins/dist/tailwindcss"],
+  plugins: ['@umijs/plugins/dist/tailwindcss'],
 });
